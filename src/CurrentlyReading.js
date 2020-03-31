5e6df3c19c3cd3193e1b83ee238022d39c6fb8ff
Andy-Selasie-Ivor <aivor3@gmail.com>
2020-03-31 12:19:14 +0000

    Add Currentlyreading component

---
+++ src/CurrentlyReading.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+let CurrentlyReading= () => {
+    return (
+        <div className="bookshelf">
+            <h2 className="bookshelf-title">Currently Reading</h2>
+        </div>
+    )
+}
+export default CurrentlyReading;
