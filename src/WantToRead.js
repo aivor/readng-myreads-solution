61a6f1b9614507977f143d9209165d92cc144b0a
Andy-Selasie-Ivor <aivor3@gmail.com>
2020-03-31 12:18:17 +0000

    Add WantToRead Component~

---
+++ src/WantToRead.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+let WantToRead=() => {
+    return (
+        <div className="bookshelf">
+            <h2 className="bookshelf-title">Want To Read</h2>
+        </div>
+    )
+}
+export default WantToRead;
