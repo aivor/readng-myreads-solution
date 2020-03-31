2a8f56a7f48929cfe898c2b34f01a1c2f679d98b
Andy-Selasie-Ivor <aivor3@gmail.com>
2020-03-31 12:16:12 +0000

    Add ReadBooks Component

---
+++ src/ReadBooks.js
@@ -0,0 +1,12 @@
+import React from 'rect';
+
+
+let ReadBooks= () => {
+    
+    return(
+        <div className="bookshelf">
+            <h2 className="bookshelf-title">Read</h2>
+        </div>
+    )
+}
+export default ReadBooks;
