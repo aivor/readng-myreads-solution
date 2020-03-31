954a752c674466bca093fa92c6891dc7b9621c80
Andy-Selasie-Ivor <aivor3@gmail.com>
2020-03-31 18:02:46 +0000

    Add State,query to SearchBook Compoent

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -1,17 +1,36 @@
 import React, { Component } from 'react';
-
+import { Link } from 'react-router-dom';
+import BookShelf from './BookShelf';
 class SearchBooks extends Component {
+    state = {
+        query: ''
+     }
+   updateQuery(query) {
+     this.setState({query})
+     this.props.searchBooks(query)
+   }
     render() {
+        const { searchResults, books} = this.props;
+        const updatedBooks = searchResults.map( book => {
+        books.map(b => {
+            if(b.id === book.id) {
+            book.shelf = b.shelf
+            }
+            return b;
+        });
+        return book;
+        })
         return(
             <div className="search-books">
                 <div className="search-books-bar">
-                    <button className="close-search">Close</button>
+                    <Link to='/'><button className="close-search">Close</button></Link>
                     <div className="search-books-input-wrapper">
-                        <input type="text" placeholder="Search by title or author" />
+                        <input type="text" placeholder="Search by title or author"
+                        value={this.state.query} onChange = { (event)=> {this.updateQuery(event.target.value)}} />
                     </div>
                 </div>
                 <div className="search-books-results">
-                    <ol className="books-grid"></ol>
+                    <BookShelf books={updatedBooks} handleChange={this.props.handleChange} />
                 </div>
             </div>
         )
